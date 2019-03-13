acef6c61372bcd643fd0427450d4ae5bb49ca014
Thanuja <me@thanuja.com>
2019-03-13 12:43:12 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import "sampleprograms/controllers"
+
+func main() {
+	//controllers.Arrayempl()
+	//controllers.ConstantsExample()
+	//controllers.StatTypeDeclExample()
+	//controllers.FuncExample()
+	//controllers.MixedVarDeclaration()
+	//controllers.ParamsExample()
+	//controllers.StringExample()
+	//controllers.SwitchExample()
+	//controllers.PointerExample()
+	//controllers.StructExample()
+	//controllers.PersonDetails()
+	//controllers.SlicesExample()
+	//controllers.MapExample()
+	//controllers.RangeExample()
+	//controllers.CallFactorial()
+	//controllers.PrintFibonaci()
+	//controllers.TypeCastExample()
+	//controllers.VariadicFuncsExample()
+	//controllers.CallClosureFunc()
+	//controllers.CallSequencer()
+	//controllers.RectMeasures()
+	//controllers.InterfaceExample()
+	//controllers.ErrorExample1()
+	//controllers.ErrorExample2()
+	//controllers.StringFunctionsExample()
+	//controllers.StringFormattingExample()
+	//controllers.JsonExample()
+	//controllers.CollectionFunctionsExample()
+	//controllers.DeferExample()
+	//controllers.JsonExample2()
+	//controllers.JsonStructExample()
+	//controllers.JsonTextExample()
+	// controllers.JsonMapExample()
+	//controllers.JSONExampleAll()
+	//controllers.DBAccessFromGo()
+
+	//controllers.StudentInfo()
+
+	//controllers.StudentInfoWithAdrs()
+
+	//controllers.DBAccessFromGoExample2()
+
+	//controllers.DBAccessFromGoExample3()
+
+	//controllers.DBManipulation()
+
+	//controllers.CreateTable()
+
+	//controllers.InsertIntoTable()
+
+	//controllers.DeleteTable()
+
+	//controllers.RetrieveFromTable()
+
+	//controllers.GORMExample()
+	//controllers.GormAnimalExample()
+	//controllers.GormUserModelExample()
+	//controllers.OneToOneRelationshipEx()
+
+	controllers.GoRoutines()
+
+}
